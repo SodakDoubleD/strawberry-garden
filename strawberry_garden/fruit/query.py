82936c1fed01398b663106123bdf14bc7e0dac4b
SodakDoubleD <sodakdoubled@gmail.com>
2021-03-23 20:28:39 -0500

    Add fruit resolver

--- strawberry_garden/fruit/query.py
+++ strawberry_garden/fruit/query.py
@@ -4,6 +4,14 @@ import strawberry
 from type import Fruit
 
 
+def get_fruits():
+    return [
+        Fruit(name='strawberry', color='red', quantity=10),
+        Fruit(name='blueberry', color='blue', quantity=20),
+        Fruit(name='kiwi', color='green', quantity=6),
+    ]
+
+
 @strawberry.type
 class Query:
-    fruits: List[Fruit]
+    fruits: List[Fruit] = strawberry.field(resolver=get_fruits)
